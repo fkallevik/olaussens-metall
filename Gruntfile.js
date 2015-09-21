f3cb7ce23966d6e9ddb1ccb273e11949be0d6ead
Fredrik Kallevik <fredrik@idrift.no>
2015-09-21 14:52:35 +0200

    Changed SCSS infrastructure again

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,13 +6,13 @@ module.exports = function(grunt) {
           loadPath: ["assets"]
         },
         files: {
-          "assets/theme.css": "assets/theme.scss"
+          "assets/theme.css": "styles/theme.scss"
         }
       },
     },
     watch: {
       styles: {
-        files: ['assets/*.scss', 'assets/styles/*.scss', 'assets/*.css'], // which files to watch
+        files: ['styles/*.scss', 'styles/partials/*.scss', 'styles/*.css'], // which files to watch
         tasks: ['sass:development'],
         options: {
           nospawn: true
